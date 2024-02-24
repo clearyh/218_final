eebb2b0b124bedd2396bd5fdbd6c0c98f0b9d39a
clearyh <155654453+clearyh@users.noreply.github.com>
2024-02-24 02:38:13 -0500

    Update gate.h

--- modules/gate/gate.h
+++ modules/gate/gate.h
@@ -7,21 +7,7 @@
 
 //=====[Declaration of public data types]======================================
 
-typedef enum {
-    GATE_CLOSED,
-    GATE_OPEN,
-    GATE_OPENING,
-    GATE_CLOSING,
-} gateStatus_t;
-
 //=====[Declarations (prototypes) of public functions]=========================
-
-void gateInit();
-
-void gateOpen();
-void gateClose();
-
-gateStatus_t gateStatusRead();
 
 //=====[#include guards - end]=================================================
 
